e873abd3f25932e2c4473e24ff1052fe8ace7eb9
6210110098 <6210110098@psu.ac.th>
2022-08-20 21:12:53 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
+const word = "Hello";
 function App() {
   return (
     <div>
-      Hello World
+      <WordCard value={word}/>
     </div>
   );
 }
